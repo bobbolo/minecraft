11cd46e0442ff51d1ea61c97a76387a2eca31b41
bobbolo <bob@houseofbob.net>
2019-07-02 21:48:19 -0600

    added java tuning and moved jvm opts to the top

--- mc_start.sh
+++ mc_start.sh
@@ -3,6 +3,7 @@ PATH=~/jre1.8.0_161/bin:$PATH
 
 MCHOME=/home/cfmain/minecraft_13
 LOOPFILE=${MCHOME}/remove_to_stop_loop.txt
+JVM_OPTS="-Xms8192M -Xmx8192M -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -Xloggc:gc.log -XX:+UseG1GC -XX:MaxGCPauseMillis=50"
 
 cd $MCHOME
 
@@ -25,7 +26,7 @@ if [ -f $LOOPFILE ]; then
   while [ -f $LOOPFILE ]
   do
     echo "${LOOPFILE} exists, restarting minecraft."
-    java -Xmx4096M -jar spigot-1.13.2.jar
+  java ${JVM_OPTS} -jar spigot-1.14.3.jar
     echo "Sleeping for 5 seconds....."
     sleep 5
   done
@@ -35,7 +36,7 @@ echo "${LOOPFILE} is gone, no longer looping....."
 else
   #since there is no loop file, just single shot the minecraft server
   echo "Starting minecraft without loop......."
-  java -Xmx4096M -jar spigot-1.13.2.jar
+  java ${JVM_OPTS} -jar spigot-1.14.3.jar
 fi
 
 echo "Script exiting...."
